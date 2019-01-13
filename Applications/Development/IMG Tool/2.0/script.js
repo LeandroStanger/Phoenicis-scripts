58ed4a0ec0af189cfb66c557edd62139fe330fea
Leandro Stanger <leandrostanger@hotmail.com>
2019-01-13 08:05:08 -0300

    Update script.js

--- Applications/Development/IMG Tool/2.0/script.js
+++ Applications/Development/IMG Tool/2.0/script.js
@@ -5,7 +5,7 @@ var installerImplementation = {
         new OnlineInstallerScript()
             .name("IMG Tool")
             .editor("gtagarage.com")
-            .applicationHomepage("https://www.gtagarage.com/mods/show.php?id=63)
+            .applicationHomepage("https://download.gtanet.com/gtagarage/files/90/imgtool20.zip?st=DkB0ekKcvBAlYudouG1vWw&e=1547377721)
             .author("Leandro Stanger")
             .url("https://www.gtagarage.com/mods/download.php?f=90")
             .checksum("9d4d1727c976a06c670dd7a00771de3f375b22ea")
